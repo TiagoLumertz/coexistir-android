cc0e8573fe5e6fd94c6668e0a8e1fc65f821f998
TiagoLumertz <tiagoplumertz@hotmail.com>
2017-10-18 20:24:54 -0200

    alterações 18 out 2017

---
+++ app/src/main/java/com/example/usurio/myapplication/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.usurio.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.iniComps();
+
+    }
+
+    private void iniComps() {
+    }
+
+}
